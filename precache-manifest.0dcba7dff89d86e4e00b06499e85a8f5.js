8b426cfdd7627b491c8947e5dd7e5a085da0ca66
Martin Alonso <martinalonso4895@gmail.com>
2020-05-07 16:14:27 -0400

    Updates

---
+++ precache-manifest.0dcba7dff89d86e4e00b06499e85a8f5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5384ae52bee1ade5b9a10a53052776e5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2b8c3a2a7c2c2a8d74c8",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "6f491a1d05b26a6ffd87",
+    "url": "/static/js/2.6cd36b3a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6cd36b3a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b8c3a2a7c2c2a8d74c8",
+    "url": "/static/js/main.d3a24764.chunk.js"
+  },
+  {
+    "revision": "cc89ab8a9ef76d631d2b",
+    "url": "/static/js/runtime-main.af12efb7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
